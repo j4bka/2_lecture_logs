760976341f6ee7c126796fcadb13a86d8d343efc
jabka <jabka@jabka.jabka>
2024-06-03 19:21:46 +0300

    header for lib

---
+++ mylib.h
@@ -0,0 +1,21 @@
+#ifdef MYLIB_H
+#define MYLIB_H
+
+#include <stdio.h>
+
+//Уровни журналирования
+#define DEBUG 0
+#define INFO 1
+#define WARN 2
+#define ERROR 3
+#define CRITICAL 4
+
+//Драйверы вывода
+#define DRIVER_CONSOLE 0
+#define DRIVER_FILE 1
+
+//Форматы сообщений 
+#define FORMAT_PLAIN 0
+#define FORMAT_JSON  1
+
+#endif
